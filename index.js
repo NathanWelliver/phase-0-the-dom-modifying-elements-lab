e2c4f9062640f52318020b04d0aca5452535ac3f
Nathan Welliver <nwelliver.564@gmail.com>
2023-06-17 22:37:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Dingus is the champion';
+
+document.body.appendChild(newHeader);
+
+global.newHeader = newHeader;
